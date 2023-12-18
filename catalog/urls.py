d3a775e8a7f6ab5749a28a38ee01709093a19d52
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2023-12-18 16:22:32 +0300

    All missions 20.1 done update 2.0

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import product_detail
 from . import views
 
 app_name = 'catalog'
@@ -7,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('contacts/submit/', views.submit_feedback, name='submit_feedback'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
