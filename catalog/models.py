c3c0f49371c3a5b82865acd495e76f41783b9ca7
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-01-17 15:39:14 +0300

    Отредактировал шаблоны и страницы html

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=50)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
