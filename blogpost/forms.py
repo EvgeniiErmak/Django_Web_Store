54281a774d99d714e1b9795597067df507479934
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-01-22 17:00:19 +0300

    Задание № 1 - выполнено

--- blogpost/forms.py
+++ blogpost/forms.py
@@ -6,4 +6,10 @@ from .models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'slug', 'content', 'preview_image', 'is_published']
+        fields = ['title', 'content', 'preview_image', 'is_published']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Проверка, есть ли поле slug в форме перед изменением виджета
+        if 'slug' in self.fields:
+            self.fields['slug'].widget = forms.HiddenInput()
